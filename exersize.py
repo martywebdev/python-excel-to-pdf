51f0f557ba8798ffb22c720bb4c1aaa09f2b8dd8
Marty <martyumlas@gmail.com>
2025-08-12 09:06:33 +0800

    creating a single PDF out of many pages

--- exersize.py
+++ exersize.py
@@ -3,21 +3,23 @@ from fpdf import FPDF
 from pathlib import Path
 from fpdf.enums import XPos, YPos
 
+pdf = FPDF("P", "mm")
+
 for filepath in Path("txt").glob("*.txt"):
-    pdf = FPDF("P", "mm")
+
     pdf.add_page()
 
     filename = filepath.stem  # filename without extension
 
     pdf.set_font("Helvetica", size=16, style='B')
-    pdf.cell(0, 10, f"{filename}", new_x=XPos.LMARGIN, new_y=YPos.NEXT)
+    pdf.cell(0, 10, f"{filename.title()}", new_x=XPos.LMARGIN, new_y=YPos.NEXT)
     pdf.ln(5)
 
     pdf.set_font("Helvetica", size=12)
     pdf.multi_cell(0, 10, filepath.read_text(encoding="utf-8"))  # read file content
 
     Path("exercises").mkdir(parents=True, exist_ok=True)
-    pdf.output(str(Path("exercises") / f"{filename}.pdf"))
+pdf.output(str(Path("exercises") / f"animals.pdf"))
 
 
 
